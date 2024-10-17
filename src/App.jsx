8495dfdf88272ddcbb2ca11007a7051155eb7f27
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2024-10-17 16:14:01 +0100

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
 import Navbar from './components/Navbar';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
 
 const App = () => {
   return (
     <>
      <Navbar />
       <HomePage />
+      <HomeCards/>
+      <JobListings/>
     </>
   )
 }
 
 export default App;
-
-
-
-
